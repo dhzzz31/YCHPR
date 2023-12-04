cf458a50a0fdfd35f71975ca450805d363cfa82c
Seniy_Jalilow Seniy_Jalilow <arseniy-3108@mail.ru>
2023-12-05 00:32:33 +0500

    вроде готово

--- Pr_1/Form1.cs
+++ Pr_1/Form1.cs
@@ -17,25 +17,32 @@ namespace Pr_1
         {
             InitializeComponent();
         }
+        // Обработчик событий для кнопки "Выход"
         private void btnExit_Click(object sender, EventArgs e)
         {
+            // Закрываем форму
             this.Close();
         }
 
+        // Обработчик событий для кнопки "Очистить"
         private void btnClear_Click(object sender, EventArgs e)
         {
+            // Очищаем текст
             txtm.Clear();
             txtn.Clear();
         }
 
+        // Обработчик событий для кнопки "Перестановка"
         private void BtnPermutation_Click(object sender, EventArgs e)
         {
             // Получаем значение из текстового поля
             int n = int.Parse(txtn.Text);
             // Вычисляем факториал числа n
             int result = Factorial(n);
+            // Отображение результата в метке
             label3.Text = result.ToString();
         }
+        // метод вычисления факториала числа
         private int Factorial(int n)
         {
             if (n == 0)
@@ -57,6 +64,7 @@ namespace Pr_1
             double result = CalculateCombination(m, n);
             if (n<m)
             {
+                // Проверяем, меньше ли n, чем m, выводим сообщение об ошибке
                 MessageBox.Show("Ошибка: n < m");
             } 
 
@@ -66,6 +74,7 @@ namespace Pr_1
             }
 
         }
+        // Вычисляем комбинацию C(m, n) и отображаем результат
         private double CalculateCombination(int m, int n)
         {
             // Вычисляем факториалы чисел m и n
@@ -77,6 +86,7 @@ namespace Pr_1
             double combination = factorialN / (factorialM * factorialNM);
             return combination;
         }
+        // Метод вычисления факториала с использованием итерации
         private double Factorial1(int number)
         {
            if (number == 0 || number == 1)
@@ -84,7 +94,8 @@ namespace Pr_1
 
            long result = 1;
 
-           for (int i = 2; i <= number; i++)
+            // Вычисляем факториал с помощью цикла
+            for (int i = 2; i <= number; i++)
            {
              result *= i;
            }
@@ -92,25 +103,32 @@ namespace Pr_1
            return result;
         }
 
+        // Обработчик событий для кнопки "Размещение"
         private void btnPlacement_Click(object sender, EventArgs e)
         {
+            // Получаем значения n и m из текстовых полей
             int n = int.Parse(txtn.Text);
             int m = int.Parse(txtm.Text);
             int result = Permutation(m, n);
+            // Проверяем, меньше ли n, чем m, выводим сообщение об ошибке
             if (n < m)
             {
                 MessageBox.Show("Ошибка: n < m");
             }
             else
             {
+                // Вычисляем перестановку P(m, n) и отображаем результат
                 label3.Text = result.ToString();
             }
         }
+        // Метод вычисления перестановки P(m, n)
         public static int Permutation(int m, int n)
         {
+            // Вычисляем перестановку, используя предоставленный метод Factorial2
             return Factorial2(n) / Factorial2(n - m);
         }
 
+        // Метод вычисления факториала с использованием рекурсии
         public static int Factorial2(int number)
         {
             if (number <= 1)
